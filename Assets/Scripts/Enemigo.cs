bdadd7ef600303c56fa0de9c302bf66af0e3fb43
PascoF <113253733+PascoF@users.noreply.github.com>
2023-10-03 21:46:38 -0500

    Added IObserver - Enemigo

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemigo : MonoBehaviour
+public class Enemigo : MonoBehaviour, IObserver
 {
     [SerializeField]
     private float speed;
